4f37079ac2e0db2d9f6531804155458dabb9be1c
Alex <strelchenko.aleksandr@yandex.ru>
2025-10-26 23:30:13 +0200

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-  // Write your code here
+  const begining = 'RSS_';
+  const result = [];
+
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith(begining)) {
+      result.push(`${key}=${value}`);
+    }
+  }
+  
+  console.log('second', result.join('; '));
 };
 
 parseEnv();
