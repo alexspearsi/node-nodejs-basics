b557cd0ad686a80b644ba2f4d7717ff12ebc9c1a
Alex <strelchenko.aleksandr@yandex.ru>
2025-10-26 23:35:56 +0200

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2);
+  const result = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i];
+    const value = args[i + 1];
+
+    const updatedKey = key.split('').slice(2).join('');
+    result.push(`${updatedKey} is ${value}`);
+  }
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
