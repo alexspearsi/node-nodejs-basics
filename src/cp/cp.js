46c75991a19fa83b9c4cb93a86e5686de302460e
Alex <strelchenko.aleksandr@yandex.ru>
2025-10-31 04:29:04 +0200

    feat: implement spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,13 +1,17 @@
 import { spawn } from 'child_process';
 import { fileURLToPath } from 'url';
 
-const PATH = fileURLToPath(new URL('./cp.js', import.meta.url));
+const PATH = fileURLToPath(new URL('./files/script.js', import.meta.url));
 
 const spawnChildProcess = async (args = []) => {
   const child = spawn('node', [PATH, ...args]);
 
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+  
   return child
 };
 
-// Put your arguments in function call to test this functionality
 spawnChildProcess(['first', 'second']);
